f3aa68c7d806f4240933340f7b599a5be3b06dd2
Steve Galbraith <steve@stevegalbraith.com>
2019-10-01 19:26:36 -0500

    Add RxCocoa

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ObservableNetworking",
-            dependencies: ["RxSwift"]),
+            dependencies: ["RxSwift", "RxCocoa"]),
         .testTarget(
             name: "ObservableNetworkingTests",
             dependencies: ["ObservableNetworking"]),
